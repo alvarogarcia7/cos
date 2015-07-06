2228f489144b758118465462653c02de271e12c1
trikitrok <trikitrok@gmail.com>
2015-07-06 19:25:24 +0200

    Removed some duplication in tests

--- src/test/java/unit_tests/users/RegistryShould.java
+++ src/test/java/unit_tests/users/RegistryShould.java
@@ -2,6 +2,7 @@ package unit_tests.users;
 
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -9,41 +10,46 @@ import users.*;
 
 
 public class RegistryShould {
-	@Rule
-	public JUnitRuleMockery context = new JUnitRuleMockery();
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
 
-	@Test
-	public void register_a_user () throws AlreadyRegisteredUserException {
-		final RegisteredUsers registeredUsers = context.mock(RegisteredUsers.class);
-        final RegistryErrorsListener errorListener = context.mock(RegistryErrorsListener.class);
-		final Registry registry = new Registry(registeredUsers, errorListener);
-		final String userName = "user_name";
+    private RegisteredUsers registeredUsers;
+    private RegistryErrorsListener errorListener;
+    private Registry registry;
 
-		context.checking(new Expectations() {{
-			oneOf (registeredUsers).add(new User(userName));
-		}});
+    @Before
+    public void setUp() {
+        registeredUsers = context.mock(RegisteredUsers.class);
+        errorListener = context.mock(RegistryErrorsListener.class);
+        registry = new Registry(registeredUsers, errorListener);
+    }
 
-		registry.register(userName);
-	}
+    @Test
+    public void register_a_user() throws AlreadyRegisteredUserException {
+        final String userName = "user_name";
 
-	@Test
-	public void not_register_an_already_registered_user () throws AlreadyRegisteredUserException {
-		final RegisteredUsers registeredUsers = context.mock(RegisteredUsers.class);
-        final RegistryErrorsListener errorListener = context.mock(RegistryErrorsListener.class);
-        final Registry registry = new Registry(registeredUsers, errorListener);
-		final String userName = "user_name";
+        context.checking(new Expectations() {{
+            oneOf(registeredUsers).add(new User(userName));
+        }});
+
+        registry.register(userName);
+    }
+
+    @Test
+    public void not_register_an_already_registered_user() throws AlreadyRegisteredUserException {
+        final String userName = "user_name";
         final User user = new User(userName);
 
-		context.checking(new Expectations() {{
-			exactly(1).of(registeredUsers).add(user);
-			exactly(1).of(registeredUsers).add(user);
+        context.checking(new Expectations() {{
+            exactly(1).of(registeredUsers).add(user);
+            exactly(1).of(registeredUsers).add(user);
             will(throwException(new AlreadyRegisteredUserException()));
 
             exactly(1).of(errorListener).alreadyRegisteredUser(userName);
-		}});
+        }});
 
-		registry.register(userName);
-		registry.register(userName);
-	}
+        registry.register(userName);
+        registry.register(userName);
+    }
 
 }
